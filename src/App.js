bb501446f4b58e45c2598cdea8f0f8d39d3b46b6
Mahima S.P <124603128+Mahimaparthiban@users.noreply.github.com>
2023-04-23 16:31:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import MyRoutes from './config/routes'
-import { Provider } from 'react-redux';
-import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faStar, faHeart, faPhone, faEnvelope, faSearch, faUtensils,
-  faThumbsUp, faSortAlphaDown, faUserMinus, faDollarSign, faAngleDoubleRight, faPlus,
-  faConciergeBell, faCommentAlt, faInfoCircle, faShoppingBasket, faTimes, faSpinner, faTruck, faTasks,
-} from '@fortawesome/free-solid-svg-icons';
+import './App.css';
+import Home from './screens/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './screens/Login';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import Signup from './screens/Signup';
+import { CartProvider } from './components/ContextReducer';
+import MyOrder from './screens/MyOrder';
 
-library.add(faStar, faHeart, faPhone, faEnvelope, faSearch, faUtensils,
-  faThumbsUp, faSortAlphaDown, faUserMinus, faDollarSign, faAngleDoubleRight, faPlus,
-  faConciergeBell, faCommentAlt, faInfoCircle, faShoppingBasket, faTimes, faSpinner, faTruck, faTasks,)
-
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
+function App() {
+  return (
+    <CartProvider>
+      <Router>
         <div>
-          <MyRoutes />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/createuser' element={<Signup />} />
+            <Route exact path='/myOrder' element={<MyOrder />} />
+          </Routes>
         </div>
-      </Provider>
-    );
-  }
+      </Router>
+    </CartProvider>
+
+  );
 }
 
 export default App;
